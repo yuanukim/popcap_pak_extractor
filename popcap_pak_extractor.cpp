6a81a5280fc904fad27ba75f7b1db9fa2be8981c
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-09-01 08:24:51 +0800

    Update popcap_pak_extractor.cpp

--- popcap_pak_extractor.cpp
+++ popcap_pak_extractor.cpp
@@ -6,184 +6,195 @@
  *   4 bytes - Magic (Should be [0xc0, 0x4a, 0xc0, 0xba])
  *   4 bytes - Version (Should be all 0) 
  *   loop 
- *       1 byte - Record Flag (exit loop if 0x80)
- *       1 byte - File name length (N) 
+ *       1 byte  - Record Flag (exit loop if 0x80)
+ *       1 byte  - File name length (N) 
  *       N bytes - Filename 
  *       4 bytes - Filesize (u32)
  *       4 bytes - Last write time (Microsoft FILETIME struct)
  *   end
  *
- *   Body
+ * Body
  *   for each record
  *       record.filesize bytes - File data
  *   end
  * 
  * This project only works for windows. try to compile it with at least C++17.
 */
+#define WIN32_LEAN_AND_MEAN
+
 #include <iostream>
-#include <algorithm>
 #include <filesystem>
 #include <string>
 #include <fstream>
 #include <vector>
 #include <array>
-#include <memory>
 #include <cstdint>
-#include <cstdlib>
 #include <Windows.h>
 
 namespace fs = std::filesystem;
 
-inline constexpr uint32_t BUF_MAX_LEN = 8192;
-inline constexpr uint32_t WINDOWS_OS_FILETIME_SIZE = sizeof(FILETIME);
+using uchar = unsigned char;
 
-struct Record {
-    unsigned char flag;
+struct FileAttr {
+    uchar flag;
     std::string fileName;
     uint32_t fileSize;
-    FILETIME lastWriteTime;   // microsoft.
-    std::string fileData;
+    FILETIME lastWriteTime;   // microsoft windows.
 };
 
-struct PopcapPak {
-    std::array<unsigned char, 4> magic;
-    std::array<unsigned char, 4> version;
-    std::vector<Record> records;
+struct PakHeader {
+    std::array<uchar, 4> magic;
+    std::array<uchar, 4> version;
+    std::vector<FileAttr> fileAttrList;
 };
 
-using PakPtr = std::shared_ptr<PopcapPak>;
-using charBuf = std::array<char, BUF_MAX_LEN>;
-using ucharBuf = std::array<unsigned char, BUF_MAX_LEN>;
+class PakFileExtractor {
+    std::ifstream pakFile;
+    PakHeader pakHeader;
+    std::array<char, 8192> buf;
+    std::array<uchar, 8192> ubuf;
 
-static charBuf buf;
-static ucharBuf ubuf;
-
-inline unsigned char decode_one_pak_byte(char c) {
-    return static_cast<unsigned char>(c ^ 0xf7);
-}
-
-inline bool is_pak_header_end(unsigned char flag) {
-    return flag == 0x80;
-}
-
-void convert_char_buf_to_unsigned(uint32_t len) {
-    for (uint32_t i = 0; i < len; ++i) {
-        ubuf[i] = decode_one_pak_byte(buf[i]);
+    uchar decode_one_pak_byte(char c) {
+        return static_cast<uchar>(c ^ 0xf7);
     }
-}
 
-void extract_magic_version(PakPtr ptr, std::ifstream& in) {
-    // parse magic, must be 0xc0, 0x4a, 0xc0, 0xba.
-    in.read(buf.data(), 4);
-    convert_char_buf_to_unsigned(4);
-    std::copy(ubuf.begin(), ubuf.begin() + 4, ptr->magic.begin());
+    bool is_pak_header_end(uchar flag) {
+        return flag == 0x80;
+    }
 
-    // parse version, must be all 0.
-    in.read(buf.data(), 4);
-    convert_char_buf_to_unsigned(4);
-    std::copy(ubuf.begin(), ubuf.begin() + 4, ptr->version.begin());
-}
+    // parse 4 btyes for magic, must be 0xc0, 0x4a, 0xc0, 0xba.
+    void parse_magic() {
+        pakFile.read(buf.data(), 4);
+        for (size_t i = 0; i < 4; ++i) {
+            pakHeader.magic[i] = decode_one_pak_byte(buf[i]);
+        }
+    }
 
-void extract_files_attributes(PakPtr ptr, std::ifstream& in) {
-    while(true) {
-        Record r;
+    // parse 4 bytes for version, must be all 0.
+    void parse_version() {
+        pakFile.read(buf.data(), 4);
+        for (size_t i = 0; i < 4; ++i) {
+            pakHeader.version[i] = decode_one_pak_byte(buf[i]);
+        }
+    }
 
-        // check if this byte means the header is end.
-        in.read(buf.data(), 1);
-        r.flag = decode_one_pak_byte(buf[0]);
-        if (is_pak_header_end(r.flag)) {
-            break;
+    // parse 1 byte for flag.
+    void parse_file_flag(FileAttr& f) {
+        pakFile.read(buf.data(), 1);
+        f.flag = decode_one_pak_byte(buf[0]);
+    }
+
+    // parse 1 byte for file name's length, then parse the file name.
+    void parse_file_name(FileAttr& f) {        
+        pakFile.read(buf.data(), 1);
+        uint8_t filenameLength = static_cast<uint8_t>(decode_one_pak_byte(buf[0]));
+
+        pakFile.read(buf.data(), filenameLength);
+        for (uint8_t i = 0; i < filenameLength; ++i) {
+            f.fileName += static_cast<char>(decode_one_pak_byte(buf[i]));
+        }
+    }
+
+    // parse 4 bytes for file size. 
+    void parse_file_size(FileAttr& f) {
+        pakFile.read(buf.data(), 4);
+
+        for (int i = 0; i < 4; ++i) {
+            ubuf[i] = decode_one_pak_byte(buf[i]);
         }
 
-        // get file name length.
-        in.read(buf.data(), 1);
-        unsigned char fileNameLengthByte = decode_one_pak_byte(buf[0]);
-        uint8_t fileNameLength = static_cast<uint8_t>(fileNameLengthByte);
-
-        // get file name.
-        in.read(buf.data(), fileNameLength);
-        convert_char_buf_to_unsigned(fileNameLength);
-        r.fileName = std::string{ reinterpret_cast<const char*>(ubuf.data()), fileNameLength };
-
-        // get file size. (uint32_t)
-        in.read(buf.data(), 4);
-        convert_char_buf_to_unsigned(4);
-        r.fileSize = *reinterpret_cast<uint32_t*>(ubuf.data());
-
-        // get last write time.
-        in.read(buf.data(), WINDOWS_OS_FILETIME_SIZE);
-        convert_char_buf_to_unsigned(WINDOWS_OS_FILETIME_SIZE);
-        r.lastWriteTime = *reinterpret_cast<FILETIME*>(ubuf.data());
-
-        ptr->records.emplace_back(r);
+        f.fileSize = *(uint32_t*)(ubuf.data());
     }
-}
 
-void extract_every_file_data(PakPtr ptr, std::ifstream& in) {
-    for (auto& r : ptr->records) {
-        uint32_t fSize = r.fileSize;
+    void parse_file_last_write_time(FileAttr& f) {
+        constexpr size_t fileTimeStructSize = sizeof(FILETIME);
+
+        pakFile.read(buf.data(), fileTimeStructSize);
+        for (size_t i = 0; i < fileTimeStructSize; ++i) {
+            ubuf[i] = decode_one_pak_byte(buf[i]);
+        }
+
+        f.lastWriteTime = *(FILETIME*)(ubuf.data());
+    }
+
+    void parse_each_file_attr() {
+        while (true) {
+            FileAttr f;
+
+            parse_file_flag(f);
+            if (is_pak_header_end(f.flag)) {
+                break;
+            }
+        
+            parse_file_name(f);
+            parse_file_size(f);
+            parse_file_last_write_time(f);
+            
+            pakHeader.fileAttrList.emplace_back(f);
+        }
+    }
+
+    void parse_and_save_one_file(FileAttr const& attr, fs::path const& p) {
+        fs::path parentPath = p.parent_path();
+        if (!fs::exists(parentPath)) {
+            fs::create_directories(parentPath);
+        }
+
+        std::ofstream out{ p, std::ios::binary };
+        uint32_t fileSize = attr.fileSize;
         uint32_t readLen;
 
-        while (fSize > 0) {
-            if (fSize < BUF_MAX_LEN) {
-                in.read(buf.data(), fSize);
+        while (fileSize > 0) {
+            if (fileSize < buf.size()) {
+                pakFile.read(buf.data(), fileSize);
             }
             else {
-                in.read(buf.data(), BUF_MAX_LEN);
+                pakFile.read(buf.data(), buf.size());
             }
 
-            readLen = in.gcount();
-            convert_char_buf_to_unsigned(readLen);
+            readLen = pakFile.gcount();
+            for (uint32_t i = 0; i < readLen; ++i) {
+                ubuf[i] = decode_one_pak_byte(buf[i]);
+            }
 
-            r.fileData += std::string{ reinterpret_cast<const char*>(ubuf.data()), readLen };
-            fSize -= readLen;
+            out.write(reinterpret_cast<const char*>(ubuf.data()), readLen);
+            fileSize -= readLen;
         }
     }
-}
 
-PakPtr extract_pak_file(std::string const& filePath) {
-    auto ptr = std::make_shared<PopcapPak>();
-    std::ifstream in{ filePath, std::ios::binary };
+    void parse_and_save_files() {
+        fs::path root_dir{ "./popcap_pak_extract_result" };
 
-    extract_magic_version(ptr, in);
-    extract_files_attributes(ptr, in);
-    extract_every_file_data(ptr, in);
-
-    return ptr;
-}
-
-void write_file_list(PakPtr ptr, std::string const& file) {
-    std::ofstream out{ file };
-
-    for (const auto& r : ptr->records) {
-        out << r.fileName << "\n";
-    }
-}
-
-void create_files(PakPtr ptr) {
-    fs::path p{ "./extract_result" };
-
-    for (const auto& r : ptr->records) {
-        fs::path temp = p / r.fileName;
-        auto dir = temp.parent_path();
-
-        if (!fs::exists(dir)) {
-            fs::create_directories(dir);
+        for (const FileAttr& attr : pakHeader.fileAttrList) {
+            parse_and_save_one_file(attr, root_dir / attr.fileName);
         }
 
-        std::ofstream out{ temp, std::ios::binary };
-        out << r.fileData;
+        pakFile.close();
     }
-}
+public:
+    PakFileExtractor(std::string const& pakFilePath) 
+        : pakFile{ pakFilePath, std::ios::binary } 
+    {}
+
+    const PakHeader& getPakHeader() const {
+        return pakHeader;
+    }
+
+    void operator()() {
+        parse_magic();
+        parse_version();
+        parse_each_file_attr();
+        parse_and_save_files();
+    }
+};
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <.pak file path>\n";
-        return EXIT_FAILURE;
+        return 0;
     }
 
-    auto ptr = extract_pak_file(argv[1]);
-    write_file_list(ptr, "./file_list.txt");
-    create_files(ptr);
-    return EXIT_SUCCESS;
+    PakFileExtractor extractor{ argv[1] };
+    extractor();
+    return 0;
 }
