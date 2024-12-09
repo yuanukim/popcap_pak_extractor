7a9186a9ca93dd3cb690e79e26ec21bd105c6606
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-12-09 17:31:20 +0800

    using unique_ptr to replace the customized cstr.

--- popcap_pak_extractor.cpp
+++ popcap_pak_extractor.cpp
@@ -28,6 +28,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <memory>
 #include <algorithm>
 #include <system_error>
 #include <utility>
@@ -38,157 +39,8 @@
 
 using uchar = unsigned char;
 
-/*
-    C++ 's std::string .data() or .c_str() is const,
-    in this program, I want to modify the underlying char* ptr,
-    and I don't want const_cast<>(), so I write this string.
-
-    the _data is always ends with '\0'.
-*/
-class CStr {
-    char* _data;
-    size_t _capacity;
-    size_t _len;
-public:
-    CStr(const char* cstr, size_t cstrLen) {
-        _len = cstrLen;
-        _capacity = cstrLen + 1;
-        _data = new char[_capacity];
-
-        std::copy(cstr, cstr + cstrLen, _data);
-        _data[_len] = '\0';
-    }
-
-    CStr(const char* cstr) : CStr{ cstr, strlen(cstr) } {}
-
-    CStr(size_t capacity) : _capacity{ capacity }, _len{ 0 } {
-        _data = new char[_capacity];
-        _data[0] = '\0';
-    }
-
-    CStr() : CStr{ 32 } {}
-
-    CStr(const CStr& other) {
-        _capacity = other._capacity;
-        _len = other._len;
-        _data = new char[other._capacity];
-
-        std::copy(other._data, other._data + other._capacity, _data);
-    }
-
-    CStr& operator=(const CStr& other) {
-        if (this != &other) {
-            _capacity = other._capacity;
-            _len = other._len;
-
-            delete _data;
-            _data = new char[other._capacity];
-            std::copy(other._data, other._data + other._capacity, _data);
-        }
-
-        return *this;
-    }
-
-    CStr(CStr&& other) noexcept {
-        _capacity = other._capacity;
-        _len = other._len;
-        _data = other._data;
-
-        other._data = nullptr;
-    }
-
-    CStr& operator=(CStr&& other) noexcept {
-        if (this != &other) {
-            _capacity = other._capacity;
-            _len = other._len;
-            std::swap(_data, other._data);
-        }
-
-        return *this;
-    }
-
-    ~CStr() noexcept {
-        if (_data != nullptr) {
-            delete _data;
-        }
-    }
-
-    size_t length() const noexcept {
-        return _len;
-    }
-
-    size_t capacity() const noexcept {
-        return _capacity;
-    }
-
-    char* data() noexcept {
-        return _data;
-    }
-
-    const char* data() const noexcept {
-        return _data;
-    }
-
-    bool empty() const noexcept {
-        return _len == 0;
-    }
-
-    char get_front() const noexcept {
-        return _data[0];
-    }
-
-    char get_back() const noexcept {
-        return _data[_len - 1];
-    }
-
-    void pop_back() noexcept {
-        if (!empty()) {
-            _len -= 1;
-        }
-    }
-
-    void expand_capacity(size_t newCapacity) {
-        if (newCapacity <= _capacity) {
-            return;
-        }
-
-        char* temp = new char[newCapacity];
-        std::copy(_data, _data + _capacity, temp);
-        delete _data;
-
-        _data = temp;
-        _capacity = newCapacity;
-    }
-
-    void expand_length(size_t newLen) {
-        expand_capacity(newLen + 1);
-        _len = newLen;
-        _data[_len] = '\0';
-    }
-
-    void append(const char* cstr, size_t cstrLen) {
-        size_t needLen = _len + cstrLen + 1;
-
-        if (needLen > _capacity) {
-            expand_capacity(needLen);
-        }
-
-        std::copy(cstr, cstr + cstrLen, _data + _len);
-        _len += cstrLen;
-        _data[_len] = '\0';
-    }
-
-    void append(const char* cstr) {
-        append(cstr, strlen(cstr));
-    }
-
-    void append(const CStr& str) {
-        append(str.data(), str.length());
-    }
-};
-
 struct FileAttr {
-    CStr fileName;
+    std::unique_ptr<char[]> fileName;
     uint32_t fileSize;
     FILETIME lastWriteTime;
 };
@@ -233,17 +85,22 @@ class HeaderParser {
         decode_bytes(header.version.data(), header.version.size());
     }
 
+    std::unique_ptr<char[]> init_file_name(size_t size) {
+        return std::unique_ptr<char[]>(new char[size]);
+    }
+
     void parse_file_name(FileAttr& attr, std::ifstream& f) {
         char c;
         f.read(&c, 1);
 
         // get the length of the file name.
         uint32_t fileNameLen = (uint32_t)decode_one_byte(c);
-        attr.fileName.expand_length(fileNameLen);
+        attr.fileName = init_file_name(fileNameLen + 1);
+        attr.fileName[fileNameLen] = '\0';
 
         // get file name.
-        f.read(attr.fileName.data(), fileNameLen);
-        decode_bytes(attr.fileName.data(), fileNameLen);
+        f.read(attr.fileName.get(), fileNameLen);
+        decode_bytes(attr.fileName.get(), fileNameLen);
     }
 
     void parse_file_size(FileAttr& attr, std::ifstream& f) {
@@ -274,7 +131,7 @@ public:
             parse_file_size(attr, f);
             parse_file_last_write_time(attr, f);
 
-            header.fileAttrList.emplace_back(attr);
+            header.fileAttrList.emplace_back(std::move(attr));
         }
     }
 };
@@ -309,8 +166,8 @@ public:
         }
     }
 
-    bool init(const CStr& path, std::error_code& ec) noexcept {
-        hFile = CreateFile(path.data(), 
+    bool init(const char* path, std::error_code& ec) noexcept {
+        hFile = CreateFile(path, 
                             GENERIC_WRITE,
                             0,
                             nullptr,
@@ -351,14 +208,14 @@ public:
     }
 };
 
-void save_file_attr_list(const Header& header, const CStr& savPath) {
-    std::ofstream out{ savPath.data() };
+void save_file_attr_list(const Header& header, const char* savPath) {
+    std::ofstream out{ savPath };
 
     for (const FileAttr& attr : header.fileAttrList) {
-        out << attr.fileName.data() << ", " << attr.fileSize << "\n";
+        out << attr.fileName.get() << ", " << attr.fileSize << "\n";
     }
 
-    std::cout << "file attributes are saved at `" << savPath.data() << "`\n";
+    std::cout << "file attributes are saved at `" << savPath << "`\n";
     std::cout << "this .pak file has " << header.fileAttrList.size() << " files\n";
 }
 
@@ -372,22 +229,39 @@ bool is_dir_exist(const char* path) {
 /*
     concatenate 2 paths, only for windows platform.
 */
-CStr path_concatenate(const CStr& parent, const CStr& sub) {
-    CStr path;
+void path_concatenate(std::array<char, MAX_PATH>& buf, const char* parent, const char* sub) {
+    bool hasBackslash = false;
+    char* cursor = buf.data();
 
-    path.expand_capacity(parent.length() + 1 + sub.length() + 1);   // like: parent/su
-    path.append(parent);
+    while (*parent != '\0') {
+        *cursor = *parent;
 
-    if (path.get_back() == '\\') {
-        path.pop_back();
+        ++cursor;
+        ++parent;
     }
 
-    if (sub.get_front() != '\\') {
-        path.append("\\");
+    --parent;
+    if (*parent == '\\') {
+        hasBackslash = true;
     }
 
-    path.append(sub);
-    return path;
+    if (*sub != '\\' && !hasBackslash) {
+        *cursor = '\\';
+        ++cursor;
+    }
+
+    if (*sub == '\\' && hasBackslash) {
+        ++sub;
+    }
+
+    while (*sub != '\0') {
+        *cursor = *sub;
+
+        ++cursor;
+        ++sub;
+    }
+
+    *cursor = '\0';
 }
 
 /*
@@ -424,14 +298,14 @@ bool construct_parent_dirs(char* path, std::error_code& ec) {
 }
 
 template<size_t N>
-void save_single_file_data(const FileAttr& attr, std::ifstream& f, std::array<char, N>& buf, CStr& filePath) {
+void save_single_file_data(const FileAttr& attr, std::ifstream& f, std::array<char, N>& buf, const char* filePath) {
     uint32_t fileSize = attr.fileSize;
     uint32_t readLen = 0;
     std::error_code ec;
     WinFile wf;
     
     if (!wf.init(filePath, ec)) {
-        std::cerr << "create file failed: `" << filePath.data() << "`, " << ec.message() << "\n";
+        std::cerr << "create file failed: `" << filePath << "`, " << ec.message() << "\n";
         return;
     }
 
@@ -447,7 +321,7 @@ void save_single_file_data(const FileAttr& attr, std::ifstream& f, std::array<ch
         decode_bytes(buf.data(), readLen);
 
         if (!wf.write_data(buf.data(), readLen, ec)) {
-            std::cerr << "write to file failed for file `" << filePath.data() << "`, " << ec.message() << "\n";
+            std::cerr << "write to file failed for file `" << filePath << "`, " << ec.message() << "\n";
             return;
         }    
         
@@ -455,26 +329,27 @@ void save_single_file_data(const FileAttr& attr, std::ifstream& f, std::array<ch
     }
 
     if (!wf.set_file_time(attr.lastWriteTime, ec)) {
-        std::cerr << "set last write time failed for file `" << filePath.data() << "`, " << ec.message() << "\n";
+        std::cerr << "set last write time failed for file `" << filePath << "`, " << ec.message() << "\n";
     }
 }
 
-void save_file_data(const Header& header, std::ifstream& f, const CStr& rootPath) {
+void save_file_data(const Header& header, std::ifstream& f, const char* rootPath) {
     std::array<char, 8192> buf;
+    std::array<char, MAX_PATH> pathBuf;
     std::error_code ec;   // C++11 's std::error_code is very fit for operating system api.
 
     for (const FileAttr& attr : header.fileAttrList) {
-        CStr filePath = path_concatenate(rootPath, attr.fileName);
+        path_concatenate(pathBuf, rootPath, attr.fileName.get());
         
-        if (!construct_parent_dirs(filePath.data(), ec)) {
-            std::cerr << "create dir failed for `" << filePath.data() << "`, " << ec.message() << "\n";
+        if (!construct_parent_dirs(pathBuf.data(), ec)) {
+            std::cerr << "create dir failed for `" << pathBuf.data() << "`, " << ec.message() << "\n";
             continue;
         }
 
-        save_single_file_data(attr, f, buf, filePath);
+        save_single_file_data(attr, f, buf, pathBuf.data());
     }
 
-    std::cout << "files data are saved at `" << rootPath.data() << "`\n";
+    std::cout << "files data are saved at `" << rootPath << "`\n";
 }
 
 int main(int argc, char* argv[]) {
